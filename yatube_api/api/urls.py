0019792a30ee2ed5e1d3a8bb9b279dce043d509e
TatyanaTkachenko <158266262+TatyanaTkachenko@users.noreply.github.com>
2025-02-16 00:27:08 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,7 +3,18 @@ from rest_framework.routers import DefaultRouter
 
 from .views import PostViewSet, GroupViewSet, CommentViewSet
 
-API_VERSION = 'v1'
+router_v1 = DefaultRouter()
+router_v1.register(r'posts', PostViewSet, basename='post')
+router_v1.register(r'groups', GroupViewSet, basename='group')
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment'
+)
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet
+
 
 router_v1 = DefaultRouter()
 router_v1.register(r'posts', PostViewSet, basename='post')
@@ -13,5 +24,5 @@ router_v1.register(
 )
 
 urlpatterns = [
-    path(f'{API_VERSION}/', include(router_v1.urls)),
+    path('v1/', include(router_v1.urls)), 
 ]
