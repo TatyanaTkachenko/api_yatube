9ebefe1b4c5679ecc2b12d46865ee1efd8d0f905
TatyanaTkachenko <158266262+TatyanaTkachenko@users.noreply.github.com>
2025-02-18 23:53:38 +0300

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -12,9 +12,13 @@ from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 class OwnerActionMixin:
     """Миксин для проверки прав владельца и управления действиями"""
 
-    def check_owner_permission(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied("У вас нет прав на выполнение этого действия!")
+def check_owner_permission(self, instance):
+    """Проверяет, является ли текущий пользователь автором объекта."""
+    if instance.author != self.request.user:
+        raise PermissionDenied(
+            "У вас нет прав на "
+            "выполнение этого действия!"
+        )
 
     def perform_update(self, serializer):
         self.check_owner_permission(serializer.instance)
