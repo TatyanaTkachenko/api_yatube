08b4cdca8882422df6e8e7dc0a27f2215662dc80
TatyanaTkachenko <158266262+TatyanaTkachenko@users.noreply.github.com>
2025-02-18 23:45:37 +0300

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -9,27 +9,23 @@ from posts.models import Post, Group
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 
 
-class OwnerPermissionMixin:
-    """Миксин для проверки прав владельца"""
-
+class OwnerActionMixin:
+    """Миксин для проверки прав владельца и управления действиями"""
+    
     def check_owner_permission(self, instance):
         if instance.author != self.request.user:
-            raise PermissionDenied(
-                "У вас нет прав на выполнение этого действия!"
-            )
+            raise PermissionDenied("У вас нет прав на выполнение этого действия!")
 
     def perform_update(self, serializer):
-        """Проверяет права владельца и сохраняет экземпляр"""
         self.check_owner_permission(serializer.instance)
         serializer.save()
 
     def perform_destroy(self, instance):
-        """Проверяет права владельца и удаляет экземпляр"""
         self.check_owner_permission(instance)
         instance.delete()
 
 
-class PostViewSet(viewsets.ModelViewSet, OwnerPermissionMixin):
+class PostViewSet(viewsets.ModelViewSet, OwnerActionMixin):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     authentication_classes = [TokenAuthentication]
@@ -44,7 +40,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = GroupSerializer
 
 
-class CommentViewSet(viewsets.ModelViewSet, OwnerPermissionMixin):
+class CommentViewSet(viewsets.ModelViewSet, OwnerActionMixin):
     serializer_class = CommentSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
